85c07c7d19063b40cd67410d924b1912ee79cafe
Fiky sigma raja roamer <01101110df@Gmail.com>
2025-02-22 22:10:13 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'Akuu punya sesuatuu buat kakaa'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
