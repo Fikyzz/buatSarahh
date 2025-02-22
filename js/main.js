96961387f84bb93fe2c9deaee6990ffc1de0e2d3
Fiky sigma raja roamer <01101110df@Gmail.com>
2025-02-22 22:05:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('love youu🤍 kwak Sarahh').split('')
+    const titles = ('lovee u 🤍 kwak Sarahh').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
